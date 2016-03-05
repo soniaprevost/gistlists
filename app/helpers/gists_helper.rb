bb9bc84bd1430cdc4437553fae2c23c25c411acd
soniaprevost <so.prevost@gmail.com>
2016-03-05 18:40:18 +0100

    Clean gists views via gists helper and partials

--- app/helpers/gists_helper.rb
+++ app/helpers/gists_helper.rb
@@ -1,2 +1,18 @@
 module GistsHelper
+
+  def gist_author(gist)
+    params[:search] ? gist.css('div.byline span.creator a').attribute('href').value.gsub!('/', '').upcase : gist.author
+  end
+
+  def gist_link(gist)
+    params[:search] ? parse_content(gist).css('a').attribute('href').value : gist.link
+  end
+
+  def gist_content(gist)
+    params[:search] ? parse_content(gist).css('div.file-box div').first : gist.content
+  end
+
+  def parse_content(gist)
+    gist.css('div.js-gist-file-update-container')
+  end
 end
